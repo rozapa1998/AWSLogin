0ddb11064b874e1981094ab16b516f730406a7ad
rozapa1998 <rodrigo1998.rz@gmail.com>
2023-04-21 12:27:50 -0300

    video

---
+++ src/components/TutorialComponent.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const TutorialComponent = () => {
+  return (
+    <div className='container'>
+        <div className='row'>
+            <div className='col-12'>
+            <iframe width="1280" height="720" src="https://www.youtube.com/embed/wYzLzXVeosw" title="AWS ACTIVACION CUENTAS" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default TutorialComponent
